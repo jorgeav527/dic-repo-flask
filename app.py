a79ddc6fa6677c7802bfc74e297621dfbb78cdcc
jorgeav527 <jorgeav527@gmail.com>
2024-11-27 21:56:12 -0500

    add

--- app.py
+++ app.py
@@ -3,9 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
-def home():
+def root():
     return render_template('index.html')
 
+@app.route("/base", methods=["GET"])
+def base():
+    return render_template('base.html')
+
+@app.route("/home", methods=["GET"])
+def home():
+    return render_template('home.html')
+
+
+@app.route("/about", methods=["GET"])
+def about():
+    return render_template('about.html')
 
 # Iniciar el servidor
 if __name__ == '__main__':
