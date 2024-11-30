b0358ab44af9731b885311867c0c57eb3a20f4b9
jorgeav527 <jorgeav527@gmail.com>
2024-11-29 21:07:48 -0500

    feat: fetch one post

--- app.py
+++ app.py
@@ -24,14 +24,20 @@ def home():
 def about():
     return render_template('about.html')
 
-
 @app.route("/post", methods=["GET"])
 def get_all_posts():
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
-    print(posts)
     conn.close()
     return render_template('post/posts.html', posts=posts)
+
+@app.route("/post/<int:post_id>", methods=["GET"])
+def get_one_posts(post_id: int):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    conn.close()
+    return render_template('post/post.html', post=post)
+
 
 # Iniciar el servidor
 if __name__ == '__main__':
