dc15bd981e4fec450f1837b3971de7c1b3e1dd49
jorgeav527 <jorgeav527@gmail.com>
2024-12-02 22:33:34 -0500

    feat: creacion de API con flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, abort
+from flask import Flask, render_template, request, redirect, url_for, abort, jsonify
 import sqlite3
 
 def get_db_connection():
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def root():
-    return render_template('index.html')
+    return jsonify({"message": "Welcome to the API"})
 
 @app.route("/base", methods=["GET"])
 def base():
@@ -29,48 +29,44 @@ def get_all_posts():
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
-    return render_template('post/posts.html', posts=posts)
+    posts_lists = [dict(post) for post in posts]
+    return jsonify(posts_lists)
 
 @app.route("/post/<int:post_id>", methods=["GET"])
 def get_one_post(post_id: int):
     conn = get_db_connection()
     post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
     conn.close()
-    return render_template('post/post.html', post=post)
-
-
-@app.route("/post/create", methods=["GET", "POST"])
-def create_one_post():
-    if request.method == "POST":
-        title = request.form["title"]
-        content = request.form["content"]
-        conn = get_db_connection()
-        conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
-        conn.commit()
-        conn.close()
-        return redirect(url_for('get_all_posts'))
-    elif request.method == "GET":
-        return render_template('post/create.html')
-
-
-@app.route("/post/edit/<int:post_id>", methods=["GET", "POST"])
-def edit_one_post(post_id):
-    conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
-    conn.close()
     if post is None:
-        abort(404)
+        abort(404, description="Post not found")
+    return jsonify(dict(post))
 
-    if request.method == "POST":
-        title = request.form["title"]
-        content = request.form["content"]
-        conn = get_db_connection()
-        conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, post_id))
-        conn.commit()
-        conn.close()
-        return redirect(url_for('get_all_posts'))
-    if request.method == "GET":
-        return render_template('post/edit.html', post=post)
+
+@app.route("/post/create", methods=["POST"])
+def create_one_post():
+    data = request.json
+    title = data.get('title')
+    content = data.get('content')
+    
+    conn = get_db_connection()
+    conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+    conn.commit()
+    conn.close()
+    return jsonify({"message": "Post created"})
+
+
+@app.route("/post/edit/<int:post_id>", methods=["PUT"])
+def edit_one_post(post_id):
+    data = request.json
+    title = data.get('title')
+    content = data.get('content')
+
+    conn = get_db_connection()
+    conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, post_id))
+    conn.commit()
+    conn.close()
+
+    return jsonify({"message": "Post updated"})
 
 
 @app.route("/post/delete/<int:post_id>", methods=["POST"])
